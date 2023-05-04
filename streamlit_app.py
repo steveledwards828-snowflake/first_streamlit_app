5d220ccafae10a5bcf421c4afc566af10f3bfaf2
steveledwards828-snowflake <132619593+steveledwards828-snowflake@users.noreply.github.com>
2023-05-04 17:40:20 -0400

    Update streamlit_app.py
    added typo test.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+streamlit.text('Breakfast Menu')
+streamlit.text('Omega 3 & Bueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text'Hard-Boiled Free-Range Egg')
