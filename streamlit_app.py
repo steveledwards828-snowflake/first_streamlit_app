31b12234940abacd3d1a00b4d295740286965e71
steveledwards828-snowflake <132619593+steveledwards828-snowflake@users.noreply.github.com>
2023-05-05 15:08:27 -0400

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
-my_cnx = snowflake.connectorconnect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select current_user(), current_account(), current_region()")
 my_data_row = my_cur.fetchone()
