f3e865a88a00d9c3bf0f46bb7850974843215ef4
steveledwards828-snowflake <132619593+steveledwards828-snowflake@users.noreply.github.com>
2023-05-05 14:09:08 -0400

    Update streamlit_app.py
    added fruityvice header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 import pandas
-import requests
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -19,6 +18,9 @@ fruits_selected = streamlit.multiselect("Pick some fruit:", list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header('Fruityvice Fruit Advice!')
+
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json());
 
