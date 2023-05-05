d06dbfec197e2cd8c49a7d34e7fddc3d46708aad
steveledwards828-snowflake <132619593+steveledwards828-snowflake@users.noreply.github.com>
2023-05-05 15:07:10 -0400

    Update streamlit_app.py
    connect to snowflake 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json());
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
+my_cnx = snowflake.connectorconnect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select current_user(), current_account(), current_region()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hellow from Snowflake:")
+streamlit.text(my_data_row)
